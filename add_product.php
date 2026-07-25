ada047a9b613641810c66cdc08916a53b5b9319b
ho4052819-prog <ho4052819@gmail.com>
2026-07-25 18:41:20 +0300

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -1,61 +1,32 @@
 <?php
+session_start();
+require './includes/connection.php';
 
-include "./includes/connection.php";
+$product_id = isset($_POST['product_id']) ? (int)$_POST['product_id'] : 0;
+$qty = isset($_POST['qty']) ? (int)$_POST['qty'] : 1;
 
-if(isset($_POST['add']))
-{
+if ($product_id > 0) {
 
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    $description = $_POST['description'];
+    $sql = "SELECT * FROM products WHERE id1 = $product_id LIMIT 1";
+    $result = mysqli_query($conn, $sql);
 
-    $image = $_FILES['image']['name'];
-    $tmp = $_FILES['image']['tmp_name'];
+    if (mysqli_num_rows($result) > 0) {
 
-    move_uploaded_file($tmp, "images/".$image);
+        if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'] = [];
+        }
 
-    $sql = "INSERT INTO products(name,price,image,description)
-            VALUES('$name','$price','$image','$description')";
+        if (isset($_SESSION['cart'][$product_id])) {
+            $_SESSION['cart'][$product_id] += $qty;
+        } else {
+            $_SESSION['cart'][$product_id] = $qty;
+        }
 
-    mysqli_query($conn,$sql);
-    echo "Product Added";
-header("location:products.php");
+        $_SESSION['message'] = "Product added successfully!";
+    } else {
+        $_SESSION['message'] = "Product not found!";
+    }
 }
 
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-
-<title>Add Product</title>
-
-<link href="./css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-<body>
-
-<div class="container mt-5">
-
-<h2>Add Product</h2>
-
-<form method="POST" enctype="multipart/form-data">
-
-<input type="text" name="name" placeholder="Name" class="form-control mb-2">
-
-<input type="text" name="price" placeholder="Price" class="form-control mb-2">
-
-<input type="text" name="description" placeholder="Description" class="form-control mb-2">
-
-<input type="file" name="image" class="form-control mb-2">
-
-<button name="add" class="btn btn-primary">
-Add Product
-</button>
-
-</form>
-
-</div>
-
-</body>
-</html>
+header("Location: cart.php");
+exit;
